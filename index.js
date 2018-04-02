f7afeaf00a394a84b95c7687634ba9071a113ce1
Dane Petersen <thegreatsunra@gmail.com>
2018-04-01 20:58:50 -0500

    Start build script by generating compiled CSS from Sass

---
+++ index.js
@@ -0,0 +1,37 @@
+const fse = require('fs-extra')
+const path = require('path')
+const sass = require('node-sass')
+
+const config = require('./config')
+
+
+const srcPath = './src'
+const distPath = config.build.outputPath
+// clear destination folder
+fse.emptyDirSync(distPath)
+
+const buildSass = () => {
+  sass.render({
+    file: path.resolve(__dirname, './styles/main.scss'),
+    includePaths: [path.resolve(__dirname, './node_modules')],
+    outputStyle: 'compressed'
+  }, (err, result) => {
+    if (!err) {
+      fse.mkdirs(path.resolve(__dirname, './dist/styles/')).then(() => {
+      // No errors during the compilation, write this result on the disk
+      fse.writeFile(path.resolve(__dirname, './dist/styles/main.css'), result.css, (err) => {
+        if (!err) {
+          console.log('\nGenerated main.css\n')
+        } else {
+          console.log('Error compiling Sass', err)
+        }
+      })
+      })
+  
+    } else {
+      console.log('Error compiling Sass', err)
+    }
+  })
+}
+
+buildSass()
