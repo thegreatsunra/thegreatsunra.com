a654da9b71156993ee2884c15ff6c189f5404715
Dane Petersen <thegreatsunra@gmail.com>
2022-09-25 08:39:36 -0500

    Start over with eslint and airbnb styles

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,15 @@
-// https://eslint.org/docs/user-guide/configuring
-
 module.exports = {
-  root: true,
-  parser: 'babel-eslint',
-  parserOptions: {
-    sourceType: 'module'
-  },
   env: {
-    node: true,
+    browser: true,
+    commonjs: true,
+    es2021: true,
   },
-  // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-  extends: [
-    'standard',
-    'eslint:recommended',
-    'plugin:node/recommended',
+  extends: 'airbnb-base',
+  overrides: [
   ],
-  plugins: [
-    'node'
-  ],
-  // add your custom rules here
-  'rules': {
-    'no-console': ['warn'],
-    'node/no-missing-require': ['warn']
-  }
-}
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+  },
+};
